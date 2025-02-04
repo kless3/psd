98b047978dad68e079e65e44b49bc6086ebde22c
HarmyFounder <hiklesd@gmail.com>
2025-02-04 14:07:58 +0300

    second time init

--- src/main/java/com/hary/testy/controllers/CatalogController.java
+++ src/main/java/com/hary/testy/controllers/CatalogController.java
@@ -9,8 +9,35 @@ public class CatalogController {
     public String getCatalog() {
         return "catalog";
     }
+
+    @GetMapping("/catalog/siding")
+    public String getSiding() {
+        return "catalog/siding";
+    }
+
+    @GetMapping("/catalog/facadePanels")
+    public String getFacadePanels() {
+        return "catalog/facadePanels";
+    }
+
+    @GetMapping("/catalog/facadeTiles")
+    public String getFacadeTiles() {
+        return "catalog/facadeTiles";
+    }
+
+    @GetMapping("/catalog/gutters")
+    public String getGutters() {
+        return "catalog/gutters";
+    }
+
+    @GetMapping("/catalog/roofing")
+    public String getRoofing() {
+        return "catalog/roofing";
+    }
+
+
     @GetMapping("/info")
-    public String getInfo(){
+    public String getInfo() {
         return "order";
     }
 }
